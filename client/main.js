3d01ef542e797d439102e0ceb4da825b4d50a780
Wayland Singh <wmsingh@ucdavis.edu>
2023-04-05 19:34:32 -0400

    initial hackathon commit

---
+++ client/main.js
@@ -0,0 +1,18 @@
+// console.log(webgazer);
+
+const timeToStop = 5000;
+
+webgazer
+  .setGazeListener(function (data, elapsedTime) {
+    if (data == null) {
+      return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+
+    if (elapsedTime >= timeToStop) {
+      webgazer.clearGazeListener();
+    }
+    console.log(xprediction, yprediction, elapsedTime); //elapsed time is based on time since begin was called
+  })
+  .begin();
